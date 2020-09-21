d522c8633e7bcdbce331a02e58dffbb54d4d936e
geemerboy <mummap@oregonstate.edu>
2020-09-21 12:28:30 -0500

    Commit

---
+++ animal.py
@@ -0,0 +1,11 @@
+# Author:  Paul Mumma
+# Date:  9/21/2020
+# Description:  Asks the user for their favorite animal and then
+#               prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
+
+
+
+
